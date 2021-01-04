2a8b7e9e73ce1da752f4b2b067f757d84c2f75eb
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2021-01-04 18:08:46 +0530

    Edited

---
+++ 02_Strings/02.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#String is like a char(Single Character) array So it has a number for each character starting with 0
+
+F_Str="Sasanka Weerakoon"
+
+print(F_Str)
+print(F_Str*5)
+print(F_Str+ '  Python Learning')
+
+#Can print the Letter you Want
+print(F_Str[0])
+print(F_Str[4])
+
+
+#Can print the letter range 
+print(F_Str[1:8])
+print(F_Str[6:])
+
