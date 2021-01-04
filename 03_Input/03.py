879795ea4e5fe3b9174df51e39e71e9e7fa7690f
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2021-01-04 18:35:33 +0530

    Create input.py

---
+++ 03_Input/03.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#You can Enter any kind of Data
+
+a= input()
+print('A =' , a)
+
+
+# In[2]:
+
+
+#To the user to enter the value you can type message inside input method
+
+a= input('Enter Your Value = ')
+print('Your value is = ' , a)
+
