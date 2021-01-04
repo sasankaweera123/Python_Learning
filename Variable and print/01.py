cdc2ded5fcd16f601e696deb79e09309798b6513
sasankaweera123 <71062887+sasankaweera123@users.noreply.github.com>
2021-01-04 17:56:08 +0530

    Create Python 01

---
+++ Variable and print/01.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+#Python has five standard data types −
+
+#1-Numbers
+#2-String
+#3-List
+#4-Tuple
+#5-Dictionary 
+
+a=1
+b=23
+c=2.5
+d='S'
+e='Hi I am Sasanka'
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+print(a,b,c,d,e)
+
+
+# In[2]:
+
+
+x=y=1
+print(x,y)
+print(y)
+
+
+# In[3]:
+
+
+x,y,z='Sasa',4,345.789
+print(x,y,z)
+print(y)
+
+
+# In[ ]:
+
+
+
+
